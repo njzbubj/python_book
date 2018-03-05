ef46fd9875de7a038d48c7e5c6ac250c42f0816b
njzbubj <taishi.m.0919@gmail.com>
2018-03-05 20:44:10 +0900

    20180305 add file

---
+++ src/ch1/bs-link.py
@@ -0,0 +1,25 @@
+'''
+Created on 2018/03/05
+
+@author: miyazawataishi
+'''
+
+from bs4 import BeautifulSoup
+
+html = """
+<html><body>
+    <ul>
+        <li><a href="http://uta.pw">uta</a></li>
+        <li><a href="http://oto.chu.jp">oto</a></li>
+    </ul>
+</body></html>
+"""
+
+soup = BeautifulSoup(html, 'html.parser')
+
+links = soup.find_all("a")
+
+for a in links:
+    href = a.attrs['href']
+    text = a.string
+    print(text, ">", href)
